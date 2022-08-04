68727d2652073e18185443dc98e58304ff4b617b
Jakob Holz <contact@jakobholz.de>
2022-08-04 22:56:21 +0200

    Changed vertex type from String to Integer
    added checkHasSource(...)

--- src/main/java/org/jajaho/main/Main.java
+++ src/main/java/org/jajaho/main/Main.java
@@ -5,6 +5,7 @@ import org.jajaho.data.DirectedTypeValuePseudograph;
 import org.jajaho.data.Edge;
 import org.jajaho.data.Tuple;
 
+import java.util.Arrays;
 import java.util.Scanner;
 import java.util.Set;
 import java.util.regex.Pattern;
@@ -20,7 +21,7 @@ public class Main {
     static Pattern no = Pattern.compile("[Nn][Oo]");
 
     public static void main(String[] args) {
-        DirectedTypeValuePseudograph<String> graph = new DirectedTypeValuePseudograph<>(Edge.class);
+        DirectedTypeValuePseudograph<Integer> graph = new DirectedTypeValuePseudograph<>(Edge.class);
 
         // Local Control Patterns
         Pattern val = Pattern.compile("(VAL)|(val)");
@@ -29,7 +30,7 @@ public class Main {
         // Input Pattern
         Pattern edge = Pattern.compile("[0-9]\s[IRG]\s[0-9]\s[0-9]");
 
-        printWelcomeMsg();
+        printStartupMsg();
 
         while (true) {
             if (sc.findInLine(esc) != null)
@@ -37,11 +38,12 @@ public class Main {
 
             if (sc.findInLine(val) != null) {
                 checkForAcyclicVertices(graph);
+                checkHasSource(graph);
                 continue;
             }
 
             if (sc.findInLine(calc) != null) {
-                String gnd = defGndNode(graph);
+
             }
 
             //System.out.println(sc.findInLine(edge));
@@ -51,22 +53,22 @@ public class Main {
         }
     }
 
-    private static void printWelcomeMsg() {
+    private static void printStartupMsg() {
         System.out.println("Nodal Analysis Command-Line Interface");
         System.out.println("");
         System.out.println("Construct the network by connecting nodes like this:");
-        System.out.println("             1 R 9 2");
-        System.out.println("             ^ ^ ^ ^");
-        System.out.println("Source Node _| | | |_ Target Node");
-        System.out.println("ComponentType _| |_ Component Value");
+        System.out.println("               1 R 9 2");
+        System.out.println("               ^ ^ ^ ^");
+        System.out.println("Source Nodal  _| | | |_ Target Nodal");
+        System.out.println(" Component Type _| |_ Component Value");
         System.out.println("");
         System.out.println("To terminate the program enter: ESC");
         System.out.println("    To validate the graph enter: VAL");
         System.out.println("To calculate the solution enter: CALC");
     }
 
-    private static void checkForAcyclicVertices(DirectedTypeValuePseudograph<String> graph) {
-        Set<String> acVertices = graph.getAcylclicVertices();
+    private static void checkForAcyclicVertices(DirectedTypeValuePseudograph<Integer> graph) {
+        Set<Integer> acVertices = graph.getAcylclicVertices();
         if (acVertices.isEmpty()) {
             System.out.println("No floating nodes detected.");
         } else {
@@ -88,12 +90,12 @@ public class Main {
         }
     }
 
-    private static String defGndNode(DirectedTypeValuePseudograph<String> graph) {
-        String gnd;
+    private static Integer defGndNode(DirectedTypeValuePseudograph<Integer> graph) {
+        Integer gnd;
         System.out.println("Define GND Nodal.");
 
         while (true) {
-            String res = sc.findInLine(Pattern.compile("[0-9]"));
+            Integer res = parseUserInput(Pattern.compile("[0-9]"));
             if (res == null) {
                 sc.nextLine();
                 continue;
@@ -110,20 +112,47 @@ public class Main {
         return gnd;
     }
 
-    private Tuple<double[][], double[]> makeSLE(DirectedTypeValuePseudograph<String> graph, String gnd) {
-        double[][] a;
-        double[] b;
+    private static boolean checkHasSource(DirectedTypeValuePseudograph<Integer> graph) {
+        for (Edge edge : graph.edgeSet()) {
+            if (graph.getEdgeType(edge).equals(Component.I) || graph.getEdgeType(edge).equals(Component.U))
+                return true;
+        }
+        System.out.println("Network has no valid supply");
+        return false;
+    }
 
-        for (String vertex: graph.vertexSet()) {
-            for (Edge edge: graph.edgesOf(vertex)) {
+    private Tuple<double[][], double[]> makeSLE(DirectedTypeValuePseudograph<Integer> graph) {
+        int n = graph.edgeSet().size() - 1;
+        double[][] a = new double[n][n];
+        double[] b = new double[n];
 
+        for(double[] row : a)
+            Arrays.fill(row, 1.0);
+
+        for (Integer vertex : graph.vertexSet()) {
+            for (Edge edge : graph.edgesOf(vertex)) {
+                switch (graph.getEdgeType(edge)) {
+                    case I:
+                        b[graph.getEdgeTarget(edge)] += graph.getEdgeWeight(edge);
+                        b[graph.getEdgeSource(edge)] -= graph.getEdgeWeight(edge);
+                        break;
+                    case R:
+
+                        break;
+                    case G:
+
+                        break;
+                    default:
+                        System.out.println("EdgeType not supported.");
+                        break;
+                }
             }
         }
 
         return null;
     }
 
-    private static void inputEdge(String input, DirectedTypeValuePseudograph<String> graph) {
+    private static void inputEdge(String input, DirectedTypeValuePseudograph<Integer> graph) {
         if (input == null) {
             System.out.println("No valid edge detected.");
             return;
@@ -133,15 +162,15 @@ public class Main {
         int typeDel = input.indexOf("\s", sourceDel + 1);
         int valueDel = input.indexOf("\s", typeDel + 1);
 
-        String source = input.substring(0, sourceDel);
+        Integer source = Integer.parseInt(input.substring(0, sourceDel));
+        Integer target = Integer.parseInt(input.substring(valueDel + 1));
         Component type = Component.valueOf(input.substring(sourceDel + 1, typeDel));
         double value = Double.parseDouble(input.substring(typeDel + 1, valueDel));
-        String target = input.substring(valueDel + 1);
 
         graph.addVertex(source);
         graph.addVertex(target);
 
-        Edge e = (Edge) graph.addEdge(source, target);
+        Edge e = graph.addEdge(source, target);
         // maybe use Supplier<E> as edgeSupplier
         graph.setEdgeType(e, type);
         graph.setEdgeWeight(e, value);
@@ -149,15 +178,26 @@ public class Main {
         System.out.println("New connection made.");
     }
 
-    private static void testGraph(DirectedTypeValuePseudograph<String> graph) {
-        graph.addVertex("0");
-        graph.addVertex("1");
-        graph.addVertex("2");
+    // returns null if the pattern couldn't be matched
+    private static Integer parseUserInput(Pattern p) {
+        Integer res;
+        try {
+            res = Integer.parseInt(sc.findInLine(p));
+        } catch (NumberFormatException e) {
+            res = null;
+        }
+        return res;
+    }
 
-        Edge e0 = (Edge) graph.addEdge("0", "1");
+    private static void testGraph(DirectedTypeValuePseudograph<Integer> graph) {
+        graph.addVertex(0);
+        graph.addVertex(1);
+        graph.addVertex(2);
+
+        Edge e0 = graph.addEdge(0, 1);
         e0.setComponentType(Component.I);
-        Edge e1 = (Edge) graph.addEdge("1", "2");
-        Edge e2 = (Edge) graph.addEdge("2", "0");
+        Edge e1 = graph.addEdge(1, 2);
+        Edge e2 = graph.addEdge(2, 0);
 
         graph.setEdgeWeight(e0, 5.6);
         graph.setEdgeWeight(e2, -5.6);
